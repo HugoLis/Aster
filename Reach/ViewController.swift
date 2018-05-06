5a0ccd18be091e8820583b3a122064e0aa33e7f6
HugoLis <hl@cin.ufpe.br>
2018-05-06 20:05:39 -0300

    first commit

--- Reach/ViewController.swift
+++ Reach/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //self.view.addSubview(UIView())
+    
+    let sceneView = self.view as! SpaceView
+    
+    sceneView.PlayBackgroundMusic()
+    
+    if let scene = GameScene0(fileNamed: "GameScene") {
+      scene.scaleMode = .aspectFill
+      sceneView.presentScene(scene)
+    }
+    
   }
 
   override func didReceiveMemoryWarning() {
